ba16c633af14e7e7a2bcd24ceb1eea0dc7c1005b
jaemin-Yoo <iamjm29@naver.com>
2024-05-09 02:07:14 +0900

    feat: 메인 함수 구현

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,3 @@
 fun main() {
-    val manager = AlgorithmManager()
-    // val msg = manager.addAlgorithm(Algorithm("구현", "IMPLEMENTATION", "www.example3.com"))
-    val msg = manager.removeAlgorithm(Algorithm("구현", "IMPLEMENTATION", "www.example3.com"))
-    println(msg)
+    IOManager().start()
 }
